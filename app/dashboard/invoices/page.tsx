91287917d4994df8ecc1bb36a8625ecfecc78e74
xuekeven <kevenchn@qq.com>
2024-12-05 21:51:36 +0800

    fix: deploy bug

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 };
 
 export default async function Page(props: {
-  searchParams?: Promise<{
+  searchParams: Promise<{
     query?: string;
     page?: string;
   }>;
